961aff0170eaf8f0bf9195a961ac4e991428a243
Slav Donchev <slav.donchev@gmail.com>
2020-12-30 10:29:38 +0200

    remove key_pair_create input var

--- variables.tf
+++ variables.tf
@@ -3,12 +3,6 @@ variable "key_name" {
   description = "Name of the AWS key pair to be created."
 }
 
-variable "key_pair_create" {
-  type        = bool
-  description = "Wether to create an AWS key pair at all."
-  default     = true
-}
-
 variable "public_key" {
   type        = string
   description = "Public key to use for the AWS key pair createion. If not provided a new TLS public/private key pair will be generated."
