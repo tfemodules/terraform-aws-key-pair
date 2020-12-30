e2d03cedf6fe4f99e4363378a9f2aff286d2c0a9
Slav Donchev <slav.donchev@gmail.com>
2020-12-30 10:40:39 +0200

    add output description

--- output.tf
+++ output.tf
@@ -4,9 +4,11 @@ output "key_pair_name" {
 }
 
 output "private_key" {
-  value = var.public_key == "" ? tls_private_key.key_pair[0].private_key_pem : ""
+  value       = var.public_key == "" ? tls_private_key.key_pair[0].private_key_pem : ""
+  description = "The private key when the key pair was created by terraform."
 }
 
 output "public_key" {
-  value = aws_key_pair.key.public_key
+  value       = aws_key_pair.key.public_key
+  description = "The public key."
 }
